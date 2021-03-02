2d5dbf0e64fe2bfee1d132a09096474d4a3f1eb4
Lemuel Castro <lemuelbercasiocastro@gmail.com>
2021-03-02 10:30:50 +0800

    Replace typeface-roboto with @fontsource/roboto

--- src/index.js
+++ src/index.js
@@ -6,7 +6,11 @@ import store from './app/store';
 import { Provider } from 'react-redux';
 import interceptors from './utils/interceptors';
 import * as serviceWorker from './serviceWorker';
-import 'typeface-roboto';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 interceptors.register();
 
